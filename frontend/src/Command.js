58318607353a132e00cf45efe9ec1101de5e93e6
Eyup3 <eyup.ozturk@outlook.de>
2021-08-07 05:34:48 +0200

    n

--- frontend/src/Command.js
+++ frontend/src/Command.js
@@ -3,6 +3,6 @@ import React from 'react'
 export default function Command({ command }) {
     console.log(`command`, command)
     return (
-        < h1 > { command.command} / { command.date.getDate()}.{ command.date.getMonth() + 1}</h1 >
+        < h1 > {command.command} / {command.date.getDate()}.{command.date.getMonth() + 1}</h1 >
     )
 }
